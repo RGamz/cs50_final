323b37943e41dd42de2826ad9b69e5ad8f9c3b7b
GamzU <rgamzat@live.com>
2024-01-23 14:57:35 +0100

    MAJOR UPDATE: daily stats are working. Missing only direct SQL connections

--- swap_ams/app.py
+++ swap_ams/app.py
@@ -1,5 +1,8 @@
+from datetime import date
+from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, session
 import pandas as pd
+
 
 # Configure application
 app = Flask(__name__)
@@ -15,9 +18,13 @@ def index():
     # dfEvening = pd.read_csv("D:/Code/CS50_Final/swap_ams/db/evening.csv", sep=",")
     dfEvening = pd.read_csv("/home/gamz/Code/SWAP/cs50_final/swap_ams/db/evening.csv", sep=",")
 
+    dfEvening['creation_day'] = pd.to_datetime(dfEvening['creation_date']).dt.date
+
+    
     # show all tickets that are not admin closure and not removed
     total_tickets_morning = len(dfMorning)
     total_tickets_evening = len(dfEvening)
+    total_tickets_difference = total_tickets_morning - total_tickets_evening
 
     # agents verif
     verif_feider_morning = len(dfMorning[(dfMorning["pseudo"]=="03 - Agent Vérification SWAP") 
@@ -33,6 +40,9 @@ def index():
     verif_all_evening = len(dfEvening[(dfEvening["pseudo"]=="03 - Agent Vérification SWAP")])
     
     verif_others_evening = verif_all_evening - verif_feider_evening
+
+    verif_feider_difference = verif_feider_morning - verif_feider_evening
+    verif_others_difference = verif_others_morning - verif_others_evening
 
 
     # agents admin
@@ -50,6 +60,8 @@ def index():
     
     admin_others_evening = admin_all_evening - admin_feider_evening
 
+    admin_feider_difference = admin_feider_morning - admin_feider_evening
+    admin_others_difference = admin_others_morning - admin_others_evening
 
     # agents tech
     tech_feider_morning  = len(dfMorning[(dfMorning["pseudo"]=="01 - Agent Technique SWAP") 
@@ -66,6 +78,9 @@ def index():
     
     tech_others_evening = tech_all_evening - tech_feider_evening
 
+    tech_feider_difference = tech_feider_morning - tech_feider_evening
+    tech_others_difference = tech_others_morning - tech_others_evening
+
 
     # agent pieces
     pieces_feider_morning = len(dfMorning[(dfMorning["pseudo"]=="04 - Agent Pièces SWAP") 
@@ -73,7 +88,7 @@ def index():
     
     pieces_all_morning = len(dfMorning[(dfMorning["pseudo"]=="04 - Agent Pièces SWAP")])
     
-    pieces_others = pieces_all_morning - pieces_feider_morning
+    pieces_others_morning = pieces_all_morning - pieces_feider_morning
 
     pieces_feider_evening = len(dfEvening[(dfEvening["pseudo"]=="04 - Agent Pièces SWAP") 
                            & (dfEvening["marque"] == "FEIDER")])
@@ -82,17 +97,31 @@ def index():
     
     pieces_others_evening = pieces_all_evening - pieces_feider_evening
 
+    pieces_feider_difference = pieces_feider_morning - pieces_feider_evening
+    pieces_others_difference = pieces_others_morning - pieces_others_evening
+
+    # qty of created tickets per day
+
+    tickets_created_today = len(dfEvening[(dfEvening["creation_day"]==date.today())])
+
+
     return render_template("index.html", 
-                           pieces_others=pieces_others, pieces_feider_morning=pieces_feider_morning, 
+                           pieces_others_morning=pieces_others_morning, pieces_feider_morning=pieces_feider_morning, 
                            pieces_others_evening=pieces_others_evening, pieces_feider_evening=pieces_feider_evening, 
+                           pieces_feider_difference=pieces_feider_difference,pieces_others_difference=pieces_others_difference,
                            tech_feider_morning=tech_feider_morning,tech_others_morning=tech_others_morning, 
                            tech_feider_evening=tech_feider_evening,tech_others_evening=tech_others_evening, 
+                           tech_feider_difference = tech_feider_difference, tech_others_difference=tech_others_difference,
                            total_tickets_morning=total_tickets_morning,
                            total_tickets_evening=total_tickets_evening,
+                           total_tickets_difference=total_tickets_difference,
                            verif_feider_morning=verif_feider_morning, verif_others_morning=verif_others_morning, 
                            verif_feider_evening=verif_feider_evening, verif_others_evening=verif_others_evening,
+                           verif_feider_difference=verif_feider_difference, verif_others_difference=verif_others_difference,
                            admin_others_morning=admin_others_morning, admin_feider_morning=admin_feider_morning, 
-                           admin_feider_evening=admin_feider_evening, admin_others_evening=admin_others_evening)
+                           admin_feider_evening=admin_feider_evening, admin_others_evening=admin_others_evening,
+                           admin_feider_difference=admin_feider_difference, admin_others_difference=admin_others_difference, 
+                           tickets_created_today=tickets_created_today)
 
 
 # следить за изменениями в количестве обработанных и необработанных тикетов на ежедневной основе с разрезом по контрактам
